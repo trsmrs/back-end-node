0cb52546b9f2c0255c1e6d0dacb3c0c2dc8e7e11
Tiago <trsminfo@gmail.com>
2024-06-29 11:07:16 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const routes = require('./routes');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+})
